6c7884fd1d374cbba15b71da7b7f0e60f4ab9867
Ignacio Valdebenito <ivm.nacho@msn.com>
2020-08-13 14:22:58 -0400

    add homepage

---
+++ docs/precache-manifest.30c212145f90f4eda19a994f66bcc57e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3f09c3e246c5857fab666f8716997fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "810c3437208dc936e82e",
+    "url": "./static/css/2.27b8aa69.chunk.css"
+  },
+  {
+    "revision": "a9095b5a3e72bb9819bf",
+    "url": "./static/css/main.eada29f1.chunk.css"
+  },
+  {
+    "revision": "810c3437208dc936e82e",
+    "url": "./static/js/2.b5151a0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b5151a0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9095b5a3e72bb9819bf",
+    "url": "./static/js/main.dcbdbaeb.chunk.js"
+  },
+  {
+    "revision": "aeb606346d83852a9e4e",
+    "url": "./static/js/runtime-main.2a6cad37.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6db7167a4ba454e5ddfb0376d50475ab.js"
+  "./precache-manifest.30c212145f90f4eda19a994f66bcc57e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
